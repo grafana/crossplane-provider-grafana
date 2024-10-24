d8efa372227badd0e23da2b6500d04c422c2c48c
Duologic <jeroen@simplistic.be>
2024-10-24 16:15:37 +0200

    fix: make generate

--- apis/cloud/v1alpha1/zz_accesspolicy_types.go
+++ apis/cloud/v1alpha1/zz_accesspolicy_types.go
@@ -129,6 +129,7 @@ type RealmInitParameters struct {
 	// (String) The identifier of the org or stack. For orgs, this is the slug, for stacks, this is the stack ID.
 	// The identifier of the org or stack. For orgs, this is the slug, for stacks, this is the stack ID.
 	// +crossplane:generate:reference:type=github.com/grafana/crossplane-provider-grafana/apis/cloud/v1alpha1.Stack
+	// +crossplane:generate:reference:extractor=github.com/grafana/crossplane-provider-grafana/config/grafana.ComputedFieldExtractor("id")
 	// +crossplane:generate:reference:refFieldName=StackRef
 	// +crossplane:generate:reference:selectorFieldName=StackSelector
 	Identifier *string `json:"identifier,omitempty" tf:"identifier,omitempty"`
@@ -168,6 +169,7 @@ type RealmParameters struct {
 	// (String) The identifier of the org or stack. For orgs, this is the slug, for stacks, this is the stack ID.
 	// The identifier of the org or stack. For orgs, this is the slug, for stacks, this is the stack ID.
 	// +crossplane:generate:reference:type=github.com/grafana/crossplane-provider-grafana/apis/cloud/v1alpha1.Stack
+	// +crossplane:generate:reference:extractor=github.com/grafana/crossplane-provider-grafana/config/grafana.ComputedFieldExtractor("id")
 	// +crossplane:generate:reference:refFieldName=StackRef
 	// +crossplane:generate:reference:selectorFieldName=StackSelector
 	// +kubebuilder:validation:Optional
