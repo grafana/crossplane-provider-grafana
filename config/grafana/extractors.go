35f691aa4886388135d7a04a5538d1258be0d21a
Duologic <jeroen@simplistic.be>
2026-01-26 11:50:36 +0100

    refactor: reconsolidate the config

--- config/grafana/extractors.go
+++ config/grafana/extractors.go
@@ -10,7 +10,7 @@ import (
 
 const (
 	// SelfPackagePath is the golang path for this package.
-	SelfPackagePath = "github.com/grafana/crossplane-provider-grafana/v2/config/namespaced/grafana"
+	SelfPackagePath = "github.com/grafana/crossplane-provider-grafana/v2/config/grafana"
 )
 
 func computedFieldExtractor(field string) string {
